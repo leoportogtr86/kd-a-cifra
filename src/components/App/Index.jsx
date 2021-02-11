c75636393bab63a5d82a14c71a92bb17f6ec3be9
leoportogtr86 <leoporto_studio@hotmail.com>
2021-02-11 09:22:14 -0300

    excluindo header

--- src/components/App/Index.jsx
+++ src/components/App/Index.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import './Index.css'
-import Header from '../Header/Index'
 import Search from '../Search/Index'
 
 
-export default ()=>{
+export default () => {
 
     return (
 
         <div>
 
-            <Header/>
-            <Search/>
-
-           
-
-            
-
-
+            <Search />
 
         </div>
     )
